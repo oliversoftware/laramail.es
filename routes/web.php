b5a5804593bac177be11caeb5590a425f83bd8f9
oliver <f.oliver90@gmail.com>
2020-04-24 12:17:55 +0200

    Enviando email desde Laravel con la API MAIL GUN Cada vez que se cambie el archivo .env hay que refrescar la cache hay que refrescar la cache de laravel con php artisan config:cache

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,27 @@
 |
 */
 
+use Illuminate\Support\Facades\Mail;
+
 Route::get('/', function () {
+
+    $datos=[
+
+        "titulo"=>'Hola alumnos de laravel',
+        "contenido"=>'Esto es una prueba de envío',
+    ];
+
+    /*
+     * SEND RECIBE 3 PARÁMETROS
+     * 1-LA VIEW email.test
+     * 2-Array que contiene la informacion
+     * 3- Una función anónima (a quien va dirigido=
+     * AL CAMBIAR EL archivo ENV hay que refrescar la
+     * cache php artisan config:cache
+     */
+    Mail::send("emails.test",$datos,function ($mensaje){
+       $mensaje->to("f.oliver90@gmail.com","Juan")->subject("ojo, mensaje importante");
+
+    });
     return view('welcome');
 });
